7f2f4b53f590ce8b402de8830eb66d0d679f7024
sso-yul <106002193+sso-yul@users.noreply.github.com>
2023-04-20 15:59:29 +0900

    Update 초록.sql
    띄어쓰기 수정

--- 초록.sql
+++ 초록.sql
@@ -1,13 +1,13 @@
 drop table if exists tb_content;
 create table tb_content
 (
-	content_no		bigint			not null	primary key,
-	content_nm		varchar(100)	not null,
+	content_no	bigint	not null	primary key,
+	content_nm	varchar(100)	not null,
 	content_info	varchar(10000)	not null,
-	content_runtime	int(500)		not null,
-	previewUrl		text			not null,
-	thumbnail		text			not null,
-	age				int				not null
+	content_runtime	int(500)	not null,
+	previewUrl	text	not null,
+	thumbnail	text	not null,
+	age	int	not null
 );
 
 drop table if exists tb_content_category;
@@ -20,12 +20,12 @@ drop table if exists tb_end_service;
 create table tb_end_service
 (
 	end_service_no	bigint	not null	primary key,
-	end_date		date	not null
+	end_date	dat	not null
 );
 
 drop table if exists tb_ott;
 create table tb_ott
 (
-	ott_no	bigint		not null	primary key,
+	ott_no	bigint	not null	primary key,
 	ott_nm	varchar(50)	not null
 );
